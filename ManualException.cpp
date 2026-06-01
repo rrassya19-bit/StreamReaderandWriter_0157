3ebcebd9134ebaa3fd16195832446a0a70d69ba3
rrassya19-bit <rrassys19@gmail.com>
2026-06-01 16:16:52 +0700

    Tambah blok catch default dan selesaikan ManualException.cpp

--- ManualException.cpp
+++ ManualException.cpp
@@ -10,10 +10,18 @@ int main()
         //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan ditampilkan" << endl;
     }
+
     //kondisi jika throw yang bertipe data integer
     catch (int a)
     {
         cout << "Pengecualian akan di eksekusi" << endl;
     }
+
+    //kondisi jika throw bertipe data selain integer
+    catch (...)
+    {
+        cout << "default pengecualian diekseskusi" << endl;
+    }
+    
     return 0;
 }
