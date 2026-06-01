5509170de84c89af6d40790b12240126fdd74299
rrassya19-bit <rrassys19@gmail.com>
2026-06-01 16:25:48 +0700

    Tambah pengecekan is_open dan selesaikan ioFileWithPath.cpp

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -14,6 +14,7 @@ int main()
 
     //membuat file dalam mode menulis
     ofstream outfile;
+
     //menunjuk ke sebuah nama file dengan ekstentsi txt
     outfile.open(NamaFile + ".txt", ios::out);
 
@@ -22,15 +23,16 @@ int main()
     //looping untuk menulis isi file
     while (true)
     {
-
         cout << "- ";
+
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
+
         //kondisi jika baris hanya berisi huruf q
         if (baris == "q") break;
+
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
-
     }
     //menutup file apabila telah selesai di tulis
     outfile.close();
@@ -40,5 +42,23 @@ int main()
 
     //menunjuk ke sebuah file dengan ekstensi .txt
     infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">=  Membuka dan membaca file" << endl;
+
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+
+        //menutup file jika telah selesai di loop
+        infile.close();
+    }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    
     return 0;
 }
