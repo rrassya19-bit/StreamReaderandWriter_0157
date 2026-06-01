bf92f126a2308abcdcc55e36100d6ee32b6af08a
rrassya19-bit <rrassys19@gmail.com>
2026-06-01 16:20:00 +0700

    Tambah mode menulis ofstream pada StreamReaderandWriter.cpp

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -6,5 +6,12 @@ using namespace std;
 int main()
 {
     string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     return 0;
 }
