f14e8051ef7c90cd22e5f7ebd4a0b03d7f8c2ac8
rrassya19-bit <rrassys19@gmail.com>
2026-06-01 16:24:01 +0700

    Tambah input nama file dari pengguna pada ioFileWithPath.cpp

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main()
 {
+    //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
     return 0;
 }
